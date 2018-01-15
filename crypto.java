1f4609b5ee4395878b15ae7c49b43783541adbca
jordi2224 <jorgehuetes@gmail.com>
2018-01-15 08:39:31 +0100

    Test de update

--- crypto.java
+++ crypto.java
@@ -13,8 +13,18 @@ public class crypto {
 		
 				KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
 				SecretKey myDesKey = keygenerator.generateKey();
-				System.out.println(myDesKey);
-				System.out.println(keygenerator);
+				
+				//System.out.println(myDesKey);
+				//System.out.println(keygenerator);
+				
+				Cipher desCipher;
+				desCipher = Cipher.getInstance("DES");
+				
+				System.out.println(desCipher);
+				
+				desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+				
+				byte[] encryptedtext = desCipher.doFinal(1234);
 				
 		}catch(Exception e){
 			
