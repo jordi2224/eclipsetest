15721e2a6121173a62584f639335d0f9b56bb114
jordi2224 <jorgehuetes@gmail.com>
2018-01-27 17:54:11 +0100

    Crypto ahora usa Strings y los formatea, Ventana tiene seleccion de archivos (al fin...)

--- Crypto.java
+++ Crypto.java
@@ -6,15 +6,21 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class Crypto {
 
+	private String formattedKey;
 	private SecretKeySpec aesKey;
+	private SecretKeySpec userKey;
 	
-	 public static byte[] encrypt(byte[] entrada, SecretKeySpec key){ //TODO que llame directamente al metodo getKey
+	public byte[] encrypt(byte[] entrada, String clave){ //Encripta los bytes entrantes con AES usando el string recibido
 	      
 		 byte[] salida = new byte[0];
 		 
+		 userKey = this.getKey(clave);
+
+		 
 		 try {
+			 
 	        Cipher cipher = Cipher.getInstance("AES");
-	        cipher.init(Cipher.ENCRYPT_MODE, key);
+	        cipher.init(Cipher.ENCRYPT_MODE, userKey);
 	        salida = cipher.doFinal(entrada);
 	        
 		 }catch(Exception e) {
@@ -27,14 +33,16 @@ public class Crypto {
 	        
 	}
 	 
-	public static byte[] decrypt(byte[] entrada, SecretKeySpec key) { //TODO que llame directamente al metodo getKey
+	public byte[] decrypt(byte[] entrada, String clave) { //Desencripta los bytes entrantes con AES usando el string recibido
 		
 		byte[] salida = new byte[0];
+		
+		userKey = this.getKey(clave);
 		
 		try {
 			
 			Cipher cipher = Cipher.getInstance("AES");
-	        cipher.init(Cipher.DECRYPT_MODE, key);
+	        cipher.init(Cipher.DECRYPT_MODE, userKey);
 	        salida = cipher.doFinal(entrada);
 			
 		}catch(Exception e) {
@@ -47,13 +55,13 @@ public class Crypto {
 		
 	}
 	
-	public SecretKeySpec getKey() { //TODO adaptar a recibir un string y devolver la clave generada
+	public SecretKeySpec getKey(String clave) { //Recibe un string de menos de 64 ch y devuelve la clave AES resultante
 		
+		formattedKey = formatKey(clave);
 		
 		try {
 			
-			String secretKey = "javamolamucho111"; //debe de medir exactamente 16, 32 o 64 bits.... horas de desbugeo para esto...
-	        aesKey = new SecretKeySpec(secretKey.getBytes(), "AES");
+	        aesKey = new SecretKeySpec(formattedKey.getBytes(), "AES");
 			
 		}catch(Exception e) {
 			
@@ -65,22 +73,69 @@ public class Crypto {
 		
 	}
 	
+	public String formatKey(String clave) { //Devuelve la clave con 1s detras para que mida 16, 32 o 64 ch
+		
+		if (clave.length() == 0) {
+			System.out.println("Password length must be greater than 0 \n");
+		}else if (clave.length() <= 16) {
+			formattedKey = clave;
+			for (int i=0; i< 16-clave.length(); i++) {
+				formattedKey = formattedKey + "1";
+				
+			}
+			//System.out.println(formattedKey + " " + formattedKey.length());
+			
+		}else if (clave.length() <= 32) {
+			formattedKey = clave;
+			for (int i=0; i< 32-clave.length(); i++) {
+				formattedKey = formattedKey + "1";
+				
+			}
+			//System.out.println(formattedKey + " " + formattedKey.length());
+			
+		}else if (clave.length() <= 64) {
+			formattedKey = clave;
+			for (int i=0; i< 64-clave.length(); i++) {
+				formattedKey = formattedKey + "1";
+				
+			}
+			//System.out.println(formattedKey + " " + formattedKey.length());
+			
+		}else if (clave.length() > 64) {
+			System.out.println("Password lenght must be lower than 64 \n");
+		}else {
+			System.out.println("Unknown error \n");
+		}
+		
+		return formattedKey;
+		
+	}
 	
 	public static void main(String[] ag) throws UnsupportedEncodingException{ 
 		
         Crypto test = new Crypto();
-        SecretKeySpec clave = test.getKey();
         
-        System.out.println(clave);
+        String miClave = "ClaveChula";
         
+        byte[] enText = test.encrypt("TESTERINOPANYVINO".getBytes(), miClave);
+        
+        byte[] deText = test.decrypt(enText, miClave);
+        
+        String textofinal = new String(deText, "UTF-8");
+        
+        System.out.println(textofinal);
+        
+        /*
         String testS = "k";
-        byte[] encriptado = encrypt(testS.getBytes(), clave);
+        byte[] encriptado = encrypt(testS.getBytes(), testKey);
         System.out.println(encriptado);
         
-        byte[] desencriptado = decrypt(encriptado, clave);
+        byte[] desencriptado = decrypt(encriptado, testKey);
         String desencriptadoS = new String(desencriptado, "UTF-8"); //TODO: Añadir handle para la excepcion
         
         System.out.println(desencriptadoS);
+        */
+        
     }
 	
 }
--- Ventana.java
+++ Ventana.java
@@ -3,9 +3,10 @@ package stega;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Arrays;
-
 import javax.swing.*;
+import javax.swing.filechooser.*;
 
 
 
@@ -15,13 +16,16 @@ public class Ventana extends JFrame implements ActionListener {
     private JLabel tCheckPass;
     private JButton boton, selFichero1, selFichero2;          
     private JPasswordField passwordField, checkPassField;
+    private JTextField file1, file2, file3;
     private JTextArea log;
+    private JFileChooser fc1;
+    private JFileChooser fc2;
     
-    private static final long serialVersionUID = 1L;    //No estoy seguro de porque esto es necesario
+    private static final long serialVersionUID = 1L;    //No estoy seguro de por que esto es necesario
     													//pero sin ello da warning
     
-    Ventana(){
-        super();
+    Ventana(){ //Constructor
+        //super();
         setWindow();
         startJComponents();
     }
@@ -37,18 +41,22 @@ public class Ventana extends JFrame implements ActionListener {
         
     }
 
-    private void startJComponents() {
+    private void startJComponents() { //TODO El log no tira, hacer su propia clase?
     	
     	//LOG{
     	
-    	log = new JTextArea(50,200);
+    	log = new JTextArea(5,2);
         log.setBounds(50, 360, 1100, 180);
         log.setEditable(false);
         log.setMargin(new Insets(5, 5, 5, 5)); //TODO Esto no va. REEEEEEEEEEEEEEE
-        JScrollPane logScrollPane = new JScrollPane(log);
+
     	
     	// }LOG
         
+        fc1 = new JFileChooser();
+        fc1.setCurrentDirectory(new File(System.getProperty("user.home"))); //TODO esto tira en windows?
+        fc2 = new JFileChooser();
+        fc2.setCurrentDirectory(new File(System.getProperty("user.home")));
         
         tPassword = new JLabel();
         tCheckPass = new JLabel();
@@ -56,12 +64,17 @@ public class Ventana extends JFrame implements ActionListener {
         tInputFile = new JLabel();
         tInputMat = new JLabel();
         tOutputFile = new JLabel();
+        file1 = new JTextField();
+        file2 = new JTextField();
+        file3 = new JTextField();
         passwordField = new JPasswordField(10);
         passwordField.addActionListener(this);
         checkPassField = new JPasswordField(10);
         checkPassField.addActionListener(this);
         
         boton = new JButton();
+        selFichero1 = new JButton();
+        selFichero2 = new JButton();
         
         tPassword.setText("Password:");    
         tPassword.setBounds(50, 215, 100, 10);
@@ -74,13 +87,22 @@ public class Ventana extends JFrame implements ActionListener {
         
         tInputFile.setText("Input file:");
         tInputFile.setBounds(50, 80, 100, 30);
+        file1.setBounds(180, 80, 300, 30);
+        selFichero1.setText("...  1");
+        selFichero1.setBounds(480, 80, 30, 30);
+        selFichero1.addActionListener(this);
         
         tInputMat.setText("Input image:");
         tInputMat.setBounds(50, 110, 100, 30);
+        file2.setBounds(180, 110, 300, 30);
+        selFichero2.setText("...  2");
+        selFichero2.setBounds(480, 110, 30, 30);
+        selFichero2.addActionListener(this);
         
         tOutputFile.setText("Output image:");
         tOutputFile.setBounds(50, 140, 100, 30);
-        
+        file3.setBounds(180, 140, 300, 30);
+
         passwordField.setBounds(175, 210, 160, 25);
         checkPassField.setBounds(175, 240, 160, 25);
         
@@ -92,28 +114,54 @@ public class Ventana extends JFrame implements ActionListener {
         this.add(tCheckPass);
         this.add(tTitleEncrypt);
         this.add(tInputFile);
+        this.add(file1);
+        this.add(selFichero1);
         this.add(tInputMat);
+        this.add(file2);
+        this.add(selFichero2);
         this.add(tOutputFile);
+        this.add(file3);
         this.add(passwordField);
         this.add(checkPassField);
         this.add(boton);
         this.add(log);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {  //TODO esto es mejor en un case, en vez de getaction getID
     	
-        char[] password = passwordField.getPassword();
-        char[] checkpass = checkPassField.getPassword();
-        
-        
-        if (Arrays.equals(password, checkpass)) {
-	        log.append("Password ok!\n");
-        }else {
+        if (e.getActionCommand().equals("Hide")) { //Boton encriptacion
         	
-	        JOptionPane.showMessageDialog(this, "Mismatch!");
+        	char[] password = passwordField.getPassword();
+            char[] checkpass = checkPassField.getPassword();
+            
+        	if (Arrays.equals(password, checkpass)) {
+    	        log.append("Password ok!\n");
+            }else {
+            	
+    	        JOptionPane.showMessageDialog(this, "Mismatch!");
 
+            }
+        	
+        }else if(e.getActionCommand().equals("...  1")){
+        	
+        	int returnVal = fc1.showOpenDialog(this);
+        	
+        	if (returnVal == JFileChooser.APPROVE_OPTION) {
+        		File selectedFile = fc1.getSelectedFile();
+        		file1.setText(selectedFile.getAbsolutePath());
+        	}
+        	
+        }else if(e.getActionCommand().equals("...  2")) {
+        	
+        	int returnVal = fc2.showOpenDialog(this);
+        	
+        	if (returnVal == JFileChooser.APPROVE_OPTION) {
+        		File selectedFile = fc2.getSelectedFile();
+        		file2.setText(selectedFile.getAbsolutePath());
+        	}
+        	
         }
+        
 	        
     }
 
